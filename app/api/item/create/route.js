1124441503b947951cc9537cc8a9fa5256cb50f0
shuhei0720 <shuhei_0720@icloud.com>
2025-03-07 21:38:17 +0900

    'mongoDBにデータを作成する処理を作成'

--- app/api/item/create/route.js
+++ app/api/item/create/route.js
@@ -1,8 +1,14 @@
 import { NextResponse } from "next/server";
 import connectDB from "@/app/utils/database";
+import { ItemModel } from "@/app/utils/schemaModels";
 
 export async function POST(request) {
-  console.log(await request.json());
-  connectDB();
-  return NextResponse.json({ message: "アイテム作成" });
+  const reqBody = await request.json();
+  try {
+    await connectDB();
+    await ItemModel.create(reqBody);
+    return NextResponse.json({ message: "アイテム作成成功" });
+  } catch (err) {
+    return NextResponse.json({ message: "アイテム作成失敗" });
+  }
 }
