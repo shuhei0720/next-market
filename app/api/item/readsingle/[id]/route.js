42bab1e37870ed41a0a7e440699fb94a7d2c5bd0
shuhei0720 <shuhei_0720@icloud.com>
2025-03-07 21:54:01 +0900

    'mongoDBの一つのデータを読み取る処理を作成'

---
+++ app/api/item/readsingle/[id]/route.js
@@ -0,0 +1,17 @@
+import connectDB from "@/app/utils/database";
+import { ItemModel } from "@/app/utils/schemaModels";
+import { NextResponse } from "next/server";
+
+export async function GET(request, context) {
+  const params = await context.params;
+  try {
+    await connectDB();
+    const singleItem = await ItemModel.findById(params.id);
+    return NextResponse.json({
+      message: "アイテム読み取り成功(シングル)",
+      singleItem: singleItem,
+    });
+  } catch (err) {
+    return NextResponse.json({ message: "アイテム読み取り失敗(シングル)" });
+  }
+}
