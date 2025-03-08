2f6f6d252023091836d7082e5639c5c3ce233872
shuhei0720 <shuhei_0720@icloud.com>
2025-03-08 12:23:35 +0900

    'アイテムを全て読み取って表示する画面を作成'

--- app/page.js
+++ app/page.js
@@ -1,8 +1,33 @@
-const ReadAllItems = () => {
+import Image from "next/image";
+import Link from "next/link";
+
+export const dynamic = "force-dynamic";
+const getAllItems = async () => {
+  const response = await fetch("http://localhost:3001/api/item/readall");
+  const jsonData = await response.json();
+  const allItems = jsonData.allItems;
+  return allItems;
+};
+const ReadAllItems = async () => {
+  const allItems = await getAllItems();
   return (
     <div>
-      <h1 className="h1-style">こんにちは</h1>
-      <h3>さようなら</h3>
+      {allItems.map((item) => (
+        <Link href="" key={item._id}>
+          <Image
+            src={item.image}
+            width={750}
+            height={500}
+            alt="item-image"
+            priority
+          />
+          <div>
+            <h2>\{item.price}</h2>
+            <h3>{item.title}</h3>
+            <p>{item.description.substring(0, 80)}...</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
